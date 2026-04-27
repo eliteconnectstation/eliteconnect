9d3ee26910129a3607394abadb7a9c1c80982aa1
eliteconnectstation <waseemsami700@gmail.com>
2026-04-27 04:29:50 +0300

    Add files via upload

---
+++ HomeController1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string role)
+        {
+            if (role == "Admin")
+                return RedirectToAction("AdminDashboard");
+
+            if (role == "Agent")
+                return RedirectToAction("AgentDashboard");
+
+            if (role == "Client")
+                return RedirectToAction("ClientDashboard");
+
+            return View();
+        }
+
+        public IActionResult AdminDashboard()
+        {
+            ViewBag.Role = "Admin";
+            return View("Dashboard");
+        }
+
+        public IActionResult AgentDashboard()
+        {
+            ViewBag.Role = "Agent";
+            return View("Dashboard");
+        }
+
+        public IActionResult ClientDashboard()
+        {
+            ViewBag.Role = "Client";
+            return View("Dashboard");
+        }
+    }
+}
